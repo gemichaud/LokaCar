571311feb5cc6fb5db98992433f14e8a04d2dcb3
bernierf <felix.bernier.87@gmail.com>
2018-04-16 14:18:52 +0200

    COUCOU 2

--- app/src/main/java/fr/eni/lokacar/AccueilActivity.java
+++ app/src/main/java/fr/eni/lokacar/AccueilActivity.java
@@ -10,5 +10,6 @@ public class AccueilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
         System.out.println("coucou");
+        System.out.println("coucou2");
     }
 }
