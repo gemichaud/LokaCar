7f7a0f483b21cc34c52cd507d3baa4a05d6da3c3
Germain <38420111+gemichaud@users.noreply.github.com>
2018-04-16 15:00:51 +0200

    BO

--- app/src/main/java/fr/eni/lokacar/BO/Gerant.java
+++ app/src/main/java/fr/eni/lokacar/BO/Gerant.java
@@ -1,4 +1,71 @@
 package fr.eni.lokacar.BO;
 
+import java.util.UUID;
+
 class Gerant {
+
+    public UUID iD;
+    public String nom;
+    public String prenom;
+    public String login;
+    public  String motDePasse;
+
+
+    public Gerant() {
+    }
+
+    public Gerant(String nom, String prenom, String login, String motDePasse) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.login = login;
+        this.motDePasse = motDePasse;
+    }
+
+    public Gerant(UUID iD, String nom, String prenom, String login, String motDePasse) {
+        this.iD = iD;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.login = login;
+        this.motDePasse = motDePasse;
+    }
+
+    public UUID getiD() {
+        return iD;
+    }
+
+    public void setiD(UUID iD) {
+        this.iD = iD;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
 }
